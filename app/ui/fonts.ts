2fd506aca1a7839c008dacbfc56b62586429a3b1
Sebastian Barrera Herrera <sebastian.barrerah@sofka.local>
2024-07-09 20:02:23 -0500

    add font lusitana

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,6 +1,6 @@
 import {Inter} from 'next/font/google'
 import {Margarine} from 'next/font/google'
-
+import {Lusitana} from 'next/font/google'
 export const margarine = Margarine({
     subsets: ['latin'],
     weight: ['400'],
@@ -11,6 +11,13 @@ export const inter = Inter({
     subsets: ['latin'], 
     weight: ['500']
 })
+
+export const lusitana = Lusitana({
+    subsets: ['latin'],
+    weight: '400'
+})
+
+
 
 
 
